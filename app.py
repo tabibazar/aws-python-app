62792a225eab5306042d321293c0407c9e04026e
reza <tabibazar@gmail.com>
2025-08-20 09:29:06 -0400

    added healthz endpoint

--- app.py
+++ app.py
@@ -8,5 +8,11 @@ def hello_world():  # put application's code here
     return 'Hello World!!'
 
 
+@app.route('/healthz')
+def healthz():
+    # Simple health check endpoint for Kubernetes or load balancers
+    return 'OK', 200
+
+
 if __name__ == '__main__':
     app.run()
