c81fff364451584919fb9ba7ca724d05e8041db8
reza <tabibazar@gmail.com>
2025-08-20 22:22:07 -0400

    whatever!

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,8 +3,7 @@ terraform {
     bucket = "502768277707-terraform-state-bucket"
     key    = "state/terraform.tfstate" # no s3:// here
     region = "ca-central-1"
-    # Optional locking approaches:
-    # dynamodb_table = "tf-locks"
-    use_lockfile = true
+    # Recommended: use a DynamoDB table for reliable state locking across builds.
+    # You can pass it at init time via: -backend-config="dynamodb_table=<your-table>"
   }
 }
