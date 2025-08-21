78e58c402af9cfb1964d6e24afce399bf487f54d
reza <tabibazar@gmail.com>
2025-08-21 10:25:57 -0400

    whatever!

--- terraform/imports.tf
+++ terraform/imports.tf
@@ -1,14 +1,4 @@
-# Attempt to automatically adopt existing Kubernetes resources into Terraform state
-# to avoid "already exists" errors on apply when resources were created previously.
-# These import blocks use the current namespace and app_name variables.
-# Terraform v1.5+ executes these imports before planning resource changes.
-
-import {
-  to = kubernetes_deployment_v1.app
-  id = "${var.namespace}/${var.app_name}"
-}
-
-import {
-  to = kubernetes_service_v1.svc
-  id = "${var.namespace}/${var.app_name}"
-}
+# Imports disabled: importing non-existent remote objects causes failures on fresh environments.
+# The buildspec performs a pre-cleanup (kubectl delete) to avoid "already exists" errors, so
+# we avoid using Terraform import blocks here. If you need to adopt pre-existing resources
+# into state, run `terraform import` manually or create a temporary imports file explicitly.
